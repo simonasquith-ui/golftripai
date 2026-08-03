eeb0ae0509c4ba7a4d338545bed2145440d74be3
simonasquith-ui <simon.asquith@smoothwalltech.ae>
2026-08-03 15:27:10 +0100

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -43,7 +43,6 @@ exports.handler = async (event) => {
 
     const session = await stripe.checkout.sessions.create({
       mode: 'subscription',
-      payment_method_types: ['card'],
       line_items: [{ price: priceId, quantity: 1 }],
       success_url: 'https://gogolftrip.co.uk/?payment=success&plan=' + plan,
       cancel_url: 'https://gogolftrip.co.uk/?payment=cancelled',
